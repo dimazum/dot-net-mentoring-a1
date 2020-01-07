eecb74bf0a1010058c553a0e89e3dfa02e52710e
dzmitry_khileuski <dzmitry_khileuski@epam.com>
2020-01-07 21:07:15 +0300

    Add message output

--- App1/App1/MainActivity.cs
+++ App1/App1/MainActivity.cs
@@ -15,7 +15,18 @@ namespace App1
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            Button btn = FindViewById<Button>(Resource.Id.button1);
+            btn.Click += Btn_Click;
         }
+
+        private void Btn_Click(object sender, System.EventArgs e)
+        {
+            TextView textView = FindViewById<TextView>(Resource.Id.textView1);
+            var msg = MyLibrary.Informer.SayHello(textView.Text);
+            textView.Text = msg;
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
