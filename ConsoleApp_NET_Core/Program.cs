9d3e3c4661853e9801ba8e452d9f06b3af9b22a9
dzmitry_khileuski <dzmitry_khileuski@epam.com>
2020-01-07 17:22:09 +0300

    init app

---
+++ ConsoleApp_NET_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_NET_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
