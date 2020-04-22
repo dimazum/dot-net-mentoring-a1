aeb04f4a731e55ecfdab65175327504325541cab
dzmitry_khileuski <dzmitry_khileuski@epam.com>
2020-04-22 20:45:47 +0300

    Fix async-await issues

--- module_9(Asynchronous Tasks)/AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
+++ module_9(Asynchronous Tasks)/AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
@@ -23,20 +23,16 @@ namespace AsyncAwait.Task2.CodeReviewChallenge.Middleware
         {   
             string path = context.Request.Path;
 
-            Task staticRegTask = Task.Run(
-                () => _statisticService.RegisterVisitAsync(path)
-                .ConfigureAwait(false)
-                .GetAwaiter().OnCompleted(UpdateHeaders));
-            Console.WriteLine(staticRegTask.Status); // just for debugging purposes
-            
-            void UpdateHeaders()
-            {
-                context.Response.Headers.Add(
-                    CustomHttpHeaders.TotalPageVisits,
-                    _statisticService.GetVisitsCountAsync(path).GetAwaiter().GetResult().ToString());
-            }
+            //Task t1 = Task.Run(() => _statisticService.RegisterVisitAsync(path)
+            //    .ContinueWith(x => _statisticService.GetVisitsCountAsync(path).Result)
+            //    .ContinueWith(y => context.Response.Headers.Add(CustomHttpHeaders.TotalPageVisits, y.Result.ToString()))
+            //);
 
-            Thread.Sleep(3000); // without this the statistic counter does not work
+            await _statisticService.RegisterVisitAsync(path);
+            var count  = await _statisticService.GetVisitsCountAsync(path);
+            context.Response.Headers.Add(CustomHttpHeaders.TotalPageVisits, count.ToString());
+
+            //Thread.Sleep(3000); // without this the statistic counter does not work
             await _next(context);
         }
     }
