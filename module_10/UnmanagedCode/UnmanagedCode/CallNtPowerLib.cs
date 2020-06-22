f64cd6f6c5afbf759d9423c82cc05d510090ccfa
dzmitry_khileuski <dzmitry_khileuski@epam.com>
2020-06-22 17:30:09 +0300

    Add implementation

---
+++ module_10/UnmanagedCode/UnmanagedCode/CallNtPowerLib.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace UnmanagedCode
+{
+    public static class CallNtPowerLib
+    {
+        [DllImport("powrprof.dll", SetLastError = true)]
+        public static extern UInt32 CallNtPowerInformation(
+            Int32 InformationLevel,
+            IntPtr lpInputBuffer,
+            UInt32 nInputBufferSize,
+            IntPtr lpOutputBuffer,
+            UInt32 nOutputBufferSize);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct PROCESSOR_POWER_INFORMATION
+    {
+        public uint Number;
+        public uint MaxMhz;
+        public uint CurrentMhz;
+        public uint MhzLimit;
+        public uint MaxIdleState;
+        public uint CurrentIdleState;
+    }
+
+    struct SYSTEM_BATTERY_STATE
+    {
+        public byte AcOnLine;
+        public byte BatteryPresent;
+        public byte Charging;
+        public byte Discharging;
+        public byte spare1;
+        public byte spare2;
+        public byte spare3;
+        public byte spare4;
+        public UInt32 MaxCapacity;
+        public UInt32 RemainingCapacity;
+        public Int32 Rate;
+        public UInt32 EstimatedTime;
+        public UInt32 DefaultAlert1;
+        public UInt32 DefaultAlert2;
+    }
+
+    public struct SYSTEM_POWER_INFORMATION
+    {
+        public ulong MaxIdlenessAllowed;
+        public ulong Idleness;
+        public ulong TimeRemaining;
+        public char CoolingMode;
+    }
+}
--- module_10/UnmanagedCode/UnmanagedCode/Program.cs
+++ module_10/UnmanagedCode/UnmanagedCode/Program.cs
@@ -1,12 +1,145 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using UnmanagedCode.enums;
 
 namespace UnmanagedCode
 {
     class Program
     {
+        [DllImport("powrprof.dll", SetLastError = true)]
+        public static extern UInt32 CallNtPowerInformation(
+            Int32 InformationLevel,
+            IntPtr lpInputBuffer,
+            UInt32 nInputBufferSize,
+            IntPtr lpOutputBuffer,
+            UInt32 nOutputBufferSize);
+
+
+        [DllImport("Powrprof.dll", SetLastError = true)]
+        static extern uint SetSuspendState(bool hibernate, bool forceCritical, bool disableWakeEvent);
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //SethibernationState();
+            //var info = GetProcesorInfo();
+            var batteryState = GetSystemBatteryState();
+            var sleepTime = GetLastSleepTime();
+            var lastWakeTime = GetLastWakeTime();
+            var powerInfo = GetSystemPowerInformation();
+        }
+
+
+        private static object GetSystemBatteryState()
+        {
+            var size = Marshal.SizeOf<SYSTEM_BATTERY_STATE>();
+            var outputBuffer = Marshal.AllocCoTaskMem(size);
+            uint retval = CallNtPowerInformation(
+                (int)POWER_INFORMATION_LEVEL.SystemBatteryState,
+                IntPtr.Zero,
+                0,
+                outputBuffer,
+                (uint)Marshal.SizeOf(typeof(SYSTEM_BATTERY_STATE))
+            );
+
+            var batteryState = Marshal.PtrToStructure(outputBuffer, typeof(SYSTEM_BATTERY_STATE));
+            return batteryState;
+        }
+
+        private static object GetProcesorInfo()
+        {
+            var size = Marshal.SizeOf<PROCESSOR_POWER_INFORMATION>();
+            var outputBuffer = Marshal.AllocCoTaskMem(size);
+
+            int procCount = Environment.ProcessorCount;
+            PROCESSOR_POWER_INFORMATION[] procInfo =
+                new PROCESSOR_POWER_INFORMATION[procCount];
+            uint retval = CallNtPowerInformation(
+                (int)POWER_INFORMATION_LEVEL.ProcessorInformation,
+                IntPtr.Zero,
+                0,
+                outputBuffer,
+                (uint)(procInfo.Length * Marshal.SizeOf(typeof(PROCESSOR_POWER_INFORMATION)))
+            );
+
+            var batteryState = Marshal.PtrToStructure(outputBuffer, typeof(PROCESSOR_POWER_INFORMATION));
+
+            return batteryState;
+        }
+
+        private static object GetLastSleepTime()
+        {
+            var size = Marshal.SizeOf<ulong>();
+            var outputBuffer = Marshal.AllocCoTaskMem(size);
+
+            uint retval = CallNtPowerInformation(
+                (int)POWER_INFORMATION_LEVEL.LastSleepTime,
+                IntPtr.Zero,
+                0,
+                outputBuffer,
+                (uint)(Marshal.SizeOf(typeof(ulong)))
+            );
+
+            var lastSleepTime = Marshal.ReadInt32(outputBuffer);
+
+            return lastSleepTime;
+        }
+
+        private static object GetLastWakeTime()
+        {
+            var size = Marshal.SizeOf<ulong>();
+            IntPtr outputBuffer = Marshal.AllocCoTaskMem(size);
+
+            uint retval = CallNtPowerInformation(
+                (int)POWER_INFORMATION_LEVEL.LastWakeTime,
+                IntPtr.Zero,
+                0,
+                outputBuffer,
+                (uint)(Marshal.SizeOf(typeof(ulong)))
+            );
+
+            var lastSleepTime = Marshal.PtrToStructure(outputBuffer, typeof(ulong));
+
+            return lastSleepTime;
+        }
+
+        private static object GetSystemPowerInformation()
+        {
+            var size = Marshal.SizeOf<SYSTEM_POWER_INFORMATION>();
+            var outputBuffer = Marshal.AllocCoTaskMem(size);
+
+            uint retval = CallNtPowerInformation(
+                (int)POWER_INFORMATION_LEVEL.SystemPowerInformation,
+                IntPtr.Zero,
+                0,
+                outputBuffer,
+                (uint)(Marshal.SizeOf(typeof(SYSTEM_POWER_INFORMATION)))
+            );
+
+            var powerInformation = Marshal.PtrToStructure(outputBuffer, typeof(SYSTEM_POWER_INFORMATION));
+
+            return powerInformation;
+        }
+
+        private static void SethibernationState()
+        {
+            var result = SetSuspendState(true, true, true);
+        }
+
+        private void ReserveHibernationFile()
+        {
+            int sizeOfInputBuffer = Marshal.SizeOf<UInt32>();
+            uint sizeOfOutputBuffer = 0;
+            IntPtr inputBuffer = Marshal.AllocHGlobal(sizeOfInputBuffer);
+
+            Marshal.WriteInt32(inputBuffer, 0);
+
+            var statusResult = CallNtPowerInformation(
+                (int)POWER_INFORMATION_LEVEL.SystemReserveHiberFile,
+                inputBuffer,
+                (uint)sizeOfInputBuffer,
+                IntPtr.Zero,
+                sizeOfOutputBuffer);
         }
     }
 }
