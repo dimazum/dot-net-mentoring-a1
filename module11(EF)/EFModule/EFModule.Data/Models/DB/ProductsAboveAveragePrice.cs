fb4a70fabd37f9e70c10ec770f46269fe504c8e1
dzmitry_khileuski <dzmitry_khileuski@epam.com>
2020-07-06 21:14:57 +0300

    init console App

---
+++ module11(EF)/EFModule/EFModule.Data/Models/DB/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFModule.Data.Models.DB
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
---
+++ module11(EF)/EFModule/EFModule.Data/Models/DB/SummaryOfSalesByQuarter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFModule.Data.Models.DB
+{
+    public partial class SummaryOfSalesByQuarter
+    {
+        public DateTime? ShippedDate { get; set; }
+        public int OrderId { get; set; }
+        public decimal? Subtotal { get; set; }
+    }
+}
