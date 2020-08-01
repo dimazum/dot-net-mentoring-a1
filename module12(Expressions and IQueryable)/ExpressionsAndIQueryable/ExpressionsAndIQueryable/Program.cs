919f5f2663f08bc7fcc2aa24c59957b4c90d0000
dzmitry_khileuski <dzmitry_khileuski@epam.com>
2020-08-01 21:37:08 +0300

    Add Task 1a

---
+++ module12(Expressions and IQueryable)/ExpressionsAndIQueryable/ExpressionsAndIQueryable/Program.cs
@@ -0,0 +1,21 @@
+﻿using MyExpressions;
+using System;
+using System.Linq.Expressions;
+
+namespace ExpressionsAndIQueryable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 15;
+
+            Expression<Func<int, int>> initialExpression = x => x * 2 + (x + 1) + (1 + x) + 10 + (x - 1);
+            var addToIncrementTransformer = new IncrementAndDecrementTransform();
+            var transformedExpression = addToIncrementTransformer.VisitAndConvert(initialExpression, "");
+
+            Console.WriteLine($"Expression: {initialExpression}\nTransformed expression: {transformedExpression}\nInvoke with number = {n}. Result = {transformedExpression.Compile().Invoke(n)}\n\n");
+
+        }
+    }
+}
