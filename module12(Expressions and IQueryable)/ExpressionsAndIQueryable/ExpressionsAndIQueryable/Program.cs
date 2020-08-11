360ac6df71270ce45cd565fe52763c938de15e76
dzmitry_khileuski <dzmitry_khileuski@epam.com>
2020-08-11 12:31:00 +0300

    Add task 1b

--- module12(Expressions and IQueryable)/ExpressionsAndIQueryable/ExpressionsAndIQueryable/Program.cs
+++ module12(Expressions and IQueryable)/ExpressionsAndIQueryable/ExpressionsAndIQueryable/Program.cs
@@ -1,5 +1,6 @@
 ﻿using MyExpressions;
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace ExpressionsAndIQueryable
@@ -8,13 +9,31 @@ namespace ExpressionsAndIQueryable
     {
         static void Main(string[] args)
         {
-            int n = 15;
+            //int n = 15;
 
-            Expression<Func<int, int>> initialExpression = x => x * 2 + (x + 1) + (1 + x) + 10 + (x - 1);
-            var addToIncrementTransformer = new IncrementAndDecrementTransform();
-            var transformedExpression = addToIncrementTransformer.VisitAndConvert(initialExpression, "");
+            //Expression<Func<int, int>> initialExpression = x => x * 2 + (x + 1) + (1 + x) + 10 + (x - 1) - (1 - x);
+            //var addToIncrementTransformer = new IncrementAndDecrementTransform();
+            //var transformedExpression = addToIncrementTransformer.VisitAndConvert(initialExpression, "");
 
-            Console.WriteLine($"Expression: {initialExpression}\nTransformed expression: {transformedExpression}\nInvoke with number = {n}. Result = {transformedExpression.Compile().Invoke(n)}\n\n");
+            //Console.WriteLine($"Expression: {initialExpression}\nTransformed expression: {transformedExpression}\nInvoke with number = {n}. Result = {transformedExpression.Compile().Invoke(n)}\n\n");
+
+
+
+
+
+            var dictionary = new Dictionary<string, int>
+            {
+                {"x", 10},
+                {"y", 20},
+                {"z", 30}
+            };
+
+
+            Expression<Func<int, int, int, double>> initExpression = (x, y, z) => x * 2 + (y + 1) + (1 + z);
+            var parameterTransformer = new ParameterToConstantTransformer(dictionary);
+            var transformed = parameterTransformer.VisitAndConvert(initExpression, "");
+
+            Console.WriteLine($"Initial expression: {initExpression}\nTransformed expression: {transformed}\nResult = {transformed.Compile().Invoke(0, 0, 0)}");
 
         }
     }
